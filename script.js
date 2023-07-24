e87e4e9b587d6bbcbf8fdb70f28f3b510f46581a
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-24 12:04:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const line = document.getElementById("line");
 let rotationAngle = 0;
 
 function rotate() {
-    rotationAngle += 1; //+1 to rotate Clockwise and -1 for antiClockwise
+    rotationAngle += 2; //+1 to rotate Clockwise and -1 for antiClockwise
     line.style.transform = `rotate(${rotationAngle}deg)`; //used string template to generate rotating angle
     requestAnimationFrame(rotate); //this is recursive call
     //to create infinite animation/rotation
